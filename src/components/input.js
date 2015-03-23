1739e6d4e0ed950adca604f0913308a47bf4dfb4
Matthew Lewis <matt@mplewis.com>
2015-03-22 23:04:11 -0500

    Implement live input forms

--- src/components/input.js
+++ src/components/input.js
@@ -8,9 +8,27 @@ var Button = ReactBootstrap.Button
 
 var Input = React.createClass({
 
+  getInitialState: function() {
+    return {
+      json: '',
+      parcelable: false
+    }
+  },
+
+  handleValue: function(event) {
+    var nextState = {}
+    nextState[event.target.name] = event.target.value
+    this.setState(nextState)
+  },
+
+  handleChecked: function(event) {
+    var nextState = {}
+    nextState[event.target.name] = event.target.checked
+    this.setState(nextState)
+  },
+
   onCompile: function() {
-    console.log('Compiling JSON');
-    Actions.requestCompile({json: 'abcd1234', parcelable: true})
+    Actions.requestCompile(this.state)
   },
 
   render: function() {
@@ -18,14 +36,18 @@ var Input = React.createClass({
       <div>
         
         <InputBS
+          name="json"
           type="textarea"
           label="Source JSON"
           placeholder="Paste your JSON here."
-          rows="10" />
+          rows="10"
+          onChange={this.handleValue} />
         
         <InputBS
+          name="parcelable"
           type="checkbox"
-          label="Make Parcelable for Android apps" />
+          label="Make Parcelable for Android apps"
+          onChange={this.handleChecked} />
         
         <Button
           bsStyle="primary"

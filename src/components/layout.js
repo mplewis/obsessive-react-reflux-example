d5f36979544b2c06e5e134bf861fc0229e9feab3
Matthew Lewis <matt@mplewis.com>
2015-04-02 21:35:02 -0500

    Start replacing highlight.js with react-highlight

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,15 +4,13 @@ var Input = require('./input')
 var Output = require('./output')
 var Faq = require('./faq')
 var Bsl = require('./bs-layout')
-var hljs = require('highlight.js')
 
 require('bootstrap/dist/css/bootstrap.min.css')
 require('highlight.js/styles/tomorrow.css')
 require('./body.styl')
 
-hljs.initHighlightingOnLoad()
-
 var Layout = React.createClass({
+
   render: function() {
     return (
       <Bsl.Container>
@@ -37,6 +35,7 @@ var Layout = React.createClass({
       </Bsl.Container>
     )
   }
+
 })
 
 module.exports = Layout

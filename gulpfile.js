657b5d527b117c6232354444cc0c08b1e86da773
Matthew Lewis <matt@mplewis.com>
2015-04-02 19:34:50 -0500

    Improve Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,15 @@ var addsrc = require('gulp-add-src')
 var ghPages = require('gulp-gh-pages')
 var browserSync = require('browser-sync')
 var del = require('del')
+var runSequence = require('run-sequence')
 
 var config = {
-  toWatch: ['src/**'],
+  paths: {
+    entryPoint: 'src/index.js',
+    indexHtml: 'src/index.html',
+    outputDir: 'build',
+    toWatch: ['src/**']
+  },
   browserSync: {server: {baseDir: 'build'}},
   webpack: {
     dev: require('./webpack.config.dev.js'),
@@ -14,39 +20,52 @@ var config = {
   }
 }
 
+// Returns a webpack pipeline that builds a bundle based on the specified webpack config
+function pipelineForWebpackConfig(webpackConfig) {
+  return gulp.src(config.paths.entryPoint)
+    .pipe(webpack(webpackConfig))
+    .pipe(addsrc.append(config.paths.indexHtml))
+    .pipe(gulp.dest(config.paths.outputDir))
+}
+
+// Delete the build folder
 gulp.task('clean', function(cb) {
   del(['build'], cb)
 })
 
-// Don't clean while the server is running, it causes crashes
+// Dirty build: Don't clean while the server is running -
+// the server crashes when it can't find the files requested
 gulp.task('build-dev-dirty', function() {
-  return gulp.src('src/index.js')
-    .pipe(webpack(config.webpack.dev))
-    .pipe(addsrc.append('src/index.html'))
-    .pipe(gulp.dest('build'))
+  return pipelineForWebpackConfig(config.webpack.dev)
 })
 
-// But clean on the first build
+// Produce a clean development build
 gulp.task('build-dev', ['clean', 'build-dev-dirty'])
 
+// Produce a clean production build
 gulp.task('build-prod', ['clean'], function() {
-  return gulp.src('src/index.js')
-    .pipe(webpack(config.webpack.prod))
-    .pipe(addsrc.append('src/index.html'))
-    .pipe(gulp.dest('build'))
+  return pipelineForWebpackConfig(config.webpack.prod)
 })
 
-gulp.task('server', ['build-dev'], function() {
+// Serve the dev build with BrowserSync
+gulp.task('server', ['build-dev-dirty'], function() {
   browserSync(config.browserSync)
 })
 
+// Watch the app files for changes, rebuild and serve
 gulp.task('watch', ['server'], function() {
   gulp.watch(config.toWatch, ['build-dev-dirty', browserSync.reload])
 })
 
+// Deploy the static site to GitHub Pages
 gulp.task('deploy', ['clean', 'build-prod'], function() {
-  return gulp.src('./build/**/*')
+  return gulp.src(config.paths.outputDir + '/**/*')
     .pipe(ghPages())
 })
 
-gulp.task('default', ['clean', 'watch'])
+// Default: Clean build and start the dev server
+// Ensure clean runs and completes before starting watcher
+// to avoid race conditions and ENOENT errors.
+gulp.task('default', function(callback) {
+  runSequence('clean', 'watch', callback)
+})
